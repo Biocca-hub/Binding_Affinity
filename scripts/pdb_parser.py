749402fab4e74b5b963bcd40c519515e12b1b47c
Bianca Gustini <bgustini96@gmail.com>
2026-04-09 12:18:34 +0200

    Extracted all chains and saved all pdbs, except 4YEB

---
+++ scripts/pdb_parser.py
@@ -0,0 +1,26 @@
+def chain_extractor(chain, pdb):
+  'Extracts a pdb chain from a pdb file'
+  chain_name = "chain/"+chain+'.pdb'
+  pdb_file = pdb+".pdb"
+  chain_id = chain[-1]
+  with open(chain_name, 'w') as writer:  
+    with open(pdb_file, 'r') as reader:
+      for line in reader:
+        if line.startswith("ATOM") and line[21] == chain_id:
+          writer.write(line)
+
+with open('chains.txt', 'r') as reader:
+    chains=''
+    for line in reader:
+        chains+=line 
+    chains = chains.split("\n")
+with open('pdb.txt', 'r') as reader:
+    pdbs=''
+    for line in reader:
+        pdbs+=line 
+    pdbs = pdbs.split(",")
+
+for el in chains:
+   if el[:-2]!='4YEB':
+       chain_extractor(el, el[:-2])
+
